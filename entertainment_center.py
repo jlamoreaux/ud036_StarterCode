109f482510979925fe60bec555d2239a7766ee27
jlamoreaux <jnacious88@gmail.com>
2017-05-19 17:21:57 -0500

    Add files via upload
    Final Version, ready to submit. Added appropriate docstrings and comments.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,44 +1,80 @@
+#!/usr/bin/env python
+
+"""entertainment_center.py: Contains database for the Fresh Tomatoes website.
+
+    Runs fresh_tomatoes.open_movies_page() to create fresh_tomatoes.html"""
+
 import media
 import fresh_tomatoes
 
-gog_vol2 = media.Movie("Guardians of the Galaxy Vol. 2",
-                       "Star Lord and his companions come face to face with Star Lord's father named Ego",
+__author__ = "Udacity Full Stack Web Developer Nanodegree, Jordan Lamoreaux"
+__credits__ = "Jordan Lamoreaux"
+
+__version__ = "1.0"
+__maintainer__ = "Jordan Lamoreaux"
+__email__ = "jnacious88@gmail.com"
+__status__ = "Development"
+
+
+#The following are the Movies contained on Fresh Tomatoes
+
+GOG_VOL2 = media.Movie("Guardians of the Galaxy Vol. 2",
+                       "Star Lord and his companions come face to face with Star Lord's "
+                       "father named Ego",
                        "http://cdn.movieweb.com/img.site/PHi3FGAu4Fkdlm_1_l.jpg",
                        "https://www.youtube.com/watch?v=duGqrYw4usE",
                        media.Movie.VALID_RATINGS[2])
 
-wonder_woman = media.Movie("Wonder Woman",
-                           "Before she was Wonder Woman she was Diana, princess of the Amazons, trained warrior.",
-                           "https://images-na.ssl-images-amazon.com/images/M/MV5BNDFmZjgyMTEtYTk5MC00NmY0LWJhZjktOWY2MzI5YjkzODNlXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_SY1000_SX675_AL_.jpg",
+WONDER_WOMAN = media.Movie("Wonder Woman",
+                           "Before she was Wonder Woman she was Diana,"
+                           "princess of the Amazons, trained warrior.",
+                           "https://images-na.ssl-images-amazon.com/images/"
+                           "M/MV5BNDFmZjgyMTEtYTk5MC00NmY0LWJhZjktOWY2MzI5Yj"
+                           "kzODNlXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_SY1000_SX675_AL_.jpg",
                            "https://www.youtube.com/watch?v=INLzqh7rZ-U",
                            media.Movie.VALID_RATINGS[2])
 
-
-thor3 = media.Movie("Thor 3 Ragnarok",
-                    "Imprisoned, the mighty Thor finds himself in a lethal gladiatorial contest against the Hulk, his former ally.",
-                    "https://images-na.ssl-images-amazon.com/images/M/MV5BMjE1ODgwOTkzNF5BMl5BanBnXkFtZTgwMDcwMTg5MTI@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+THOR3 = media.Movie("Thor 3 Ragnarok",
+                    "Imprisoned, the mighty Thor finds himself in a lethal "
+                    "gladiatorial contest against the Hulk, his former ally.",
+                    "https://images-na.ssl-images-amazon.com/images/M/"
+                    "MV5BMjE1ODgwOTkzNF5BMl5BanBnXkFtZTgwMDcwMTg5MTI@."
+                    "_V1_SY1000_CR0,0,674,1000_AL_.jpg",
                     "https://www.youtube.com/watch?v=v7MGUNV8MxU",
                     media.Movie.VALID_RATINGS[2])
 
-
-spiderman_homecoming = media.Movie("Spider-Man: Homecoming",
-                                   "Peter Parker attempts to balance his life in high school with his career as the web-slinging superhero Spider-Man",
-                                   "https://images-na.ssl-images-amazon.com/images/M/MV5BNTk4ODQ1MzgzNl5BMl5BanBnXkFtZTgwMTMyMzM4MTI@._V1_SY1000_CR0,0,658,1000_AL_.jpg",
+SPIDERMAN_HOMECOMING = media.Movie("Spider-Man: Homecoming",
+                                   "Peter Parker attempts to balance his life in high school "
+                                   "with his career as the web-slinging superhero Spider-Man",
+                                   "https://images-na.ssl-images-amazon.com/images/M/"
+                                   "MV5BNTk4ODQ1MzgzNl5BMl5BanBnXkFtZTgwMTMyMzM4MTI@."
+                                   "_V1_SY1000_CR0,0,658,1000_AL_.jpg",
                                    "https://www.youtube.com/watch?v=39udgGPyYMg",
                                    media.Movie.VALID_RATINGS[2])
 
-justice_league = media.Movie("Justice League",
-                             "Fueled by his restored faith in humanity and inspired by Superman's selfless act, Bruce Wayne enlists the help of his newfound ally, Diana Prince, to face an even greater enemy.",
-                             "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI2NjI2MDQ0NV5BMl5BanBnXkFtZTgwMTc1MjAwMjI@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+JUSTICE_LEAGUE = media.Movie("Justice League",
+                             "Fueled by his restored faith in humanity and inspired by Superman's selfless act, "
+                             "Bruce Wayne enlists the help of his newfound ally, Diana Prince, "
+                             "to face an even greater enemy.",
+                             "https://images-na.ssl-images-amazon.com/images/M/"
+                             "MV5BMjI2NjI2MDQ0NV5BMl5BanBnXkFtZTgwMTc1MjAwMjI@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
                              "https://www.youtube.com/watch?v=3cxixDgHUYw",
                              media.Movie.VALID_RATINGS[2])
 
-sw_the_last_jedi = media.Movie("Star Wars: The Last Jedi",
-                               "Rey continues her epic journey with Finn, Poe and Luke Skywalker in the next chapter of the saga.",
-                               "https://images-na.ssl-images-amazon.com/images/M/MV5BOTE5NzYyNjM0Ml5BMl5BanBnXkFtZTgwNjk4MDIwMjI@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+SW_THE_LAST_JEDI = media.Movie("Star Wars: The Last Jedi",
+                               "Rey continues her epic journey with Finn, Poe and "
+                               "Luke Skywalker in the next chapter of the saga.",
+                               "https://images-na.ssl-images-amazon.com/images/M/"
+                               "MV5BOTE5NzYyNjM0Ml5BMl5BanBnXkFtZTgwNjk4MDIwMjI@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
                                "https://www.youtube.com/watch?v=zB4I68XVPzQ",
                                media.Movie.VALID_RATINGS[2])
 
-movies = [sw_the_last_jedi, wonder_woman, thor3, spiderman_homecoming, justice_league, gog_vol2]
+
+# pylint recommends 'movies' being in formatted differently. I believe it
+# views it as a GLOBAL_CONSTANT so it should be in ALL CAPS, but for
+# consistancy with fresh_tomatoes, 'movies' was left lowercase
+movies = [SW_THE_LAST_JEDI, WONDER_WOMAN, THOR3, SPIDERMAN_HOMECOMING,
+          JUSTICE_LEAGUE, GOG_VOL2]
+
 
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,23 +1,64 @@
+#!/usr/bin/env python
+
+"""media.py: Module containing classes refered to by entertainment_center.py.
+
+    Part of the Fresh Tomatoes project."""
+
 import webbrowser
+
+__author__ = "Udacity Full Stack Web Developer Nanodegree, Jordan Lamoreaux"
+__credits__ = "Jordan Lamoreaux"
+
+__version__ = "1.0"
+__maintainer__ = "Jordan Lamoreaux"
+__email__ = "jnacious88@gmail.com"
+__status__ = "Development"
 
 
 class Video():
+    """This is the Parent Class with attributes that Movie and TvShow inheret.
+
+    Attributes:
+        title: A string containing the title of the movie.
+        storyline: A string containing a summary of the movie's story line.
+        image: A string containing the URL to the movie poster.
+    """
+
     def __init__(self, title, storyline, image):
+        """Inits Video with title, storyline and image"""
         self.title = title
         self.storyline = storyline
         self.poster_image_url = image
 
 class Movie(Video):
+    """Movie stores details of movies to display on the Fresh Tomatoes Website.
+
+    Attributes:
+        Inherets title, storyline and image from Video.
+        trailer_youtube: A string containing the URL for the movie trailer.
+        rating: A predefined variable that is selected from the list 'VALID_RATINGS'.
+            Indicates movie's MPAA Rating.
+    """
+
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_youtube, rating):
+        """Inits Movie class with movie_title, movie_storyline, poster_image_url,
+            trailer_youtube and rating."""
         Video.__init__(self, movie_title, movie_storyline, poster_image_url)
         self.trailer_youtube_url = trailer_youtube
         self.rating = rating
 
     def show_trailer(self):
+        """Plays movie trailer from YouTube"""
         webbrowser.open(self.trailer_youtube_url)
 
 class TvShow(Video):
+    """TvShow stores details of TV Shows to display on the Fresh Tomatoes Website.
+
+    Attributes:
+        Inherets title, storyline and image from Video.
+        number_of_seasons: An integer count of the total number of seasons TV Show ran"""
+
     def __init__(self, title, storyline, image, number_of_seasons):
         Video.__init__(self, title, storyline, image)
         self.seasons = number_of_seasons
